790d6bff9fa14f3356cd79325eb8ac9882b6d25c
Kalindu Bihan Fernando <99331732+KalinduBihan@users.noreply.github.com>
2024-05-14 00:40:42 +0530

    LearnerService backend added

---
+++ backend/LearnerService/models/courseModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    courseCode: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    file: {
+      type:String,
+      required:[true, 'Provide an image'],
+  }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Course", courseSchema);
