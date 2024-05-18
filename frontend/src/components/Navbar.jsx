9bce52dadfabedcd2d69e82a0404a269b7364f23
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2024-05-19 01:01:14 +0530

    Nortification service added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
-import { ImCross } from 'react-icons/im';
-import '../App.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { ImCross } from "react-icons/im";
+import "../App.css";
 
 const Navbar = () => {
   const { logout } = useLogout();
@@ -12,55 +12,89 @@ const Navbar = () => {
   const [pressed, setPress] = useState(false);
 
   const handleClick = () => {
-    logout()
-  }
+    logout();
+  };
 
   return (
-    <div className="bg-dark text-white p-4"> {/* Set background color here */}
-      <div className="w-4/10 mx-auto flex flex-row justify-end items-center"> {/* Align items to the left */}
-        <div className="flex font-poppins font-semibold space-x-10"> {/* Space between items */}
+    <div className="bg-dark text-black p-4">
+      {" "}
+      {/* Set background color here */}
+      <div className="w-4/10 mx-auto flex flex-row justify-end items-center">
+        {" "}
+        {/* Align items to the left */}
+        <div className="flex font-poppins font-semibold space-x-10">
+          {" "}
+          {/* Space between items */}
           <Link to="/">
-            <p className="hover:text-[#dede50] transition-all duration-200 ease-in">Home</p>
+            <p className="hover:text-[#dede50] transition-all duration-200 ease-in">
+              Home
+            </p>
           </Link>
-
+          <Link to="/enrollment">
+            <p className="hover:text-[#dede50] transition-all duration-200 ease-in">
+              My Learning
+            </p>
+          </Link>
+          <Link to="/cart">
+            <p className="hover:text-[#dede50] transition-all duration-200 ease-in">
+              Cart
+            </p>
+          </Link>
           <nav>
             {user && (
-                <div className="flex font-poppins font-semibold space-x-10">
-                  <span>{user.email}</span>
-                  <button onClick={handleClick}>Log out</button>
-                </div>
+              <div className="flex font-poppins font-semibold space-x-10">
+                <span>{user.email}</span>
+                <button onClick={handleClick}>Log out</button>
+              </div>
             )}
             {!user && (
-                <div className="flex font-poppins font-semibold space-x-10">
-                  <Link to="/login">
-                    <p className="hover:text-[#dede50] transition-all duration-200 ease-in">Login</p>
-                  </Link>
-                  <Link to="/signup">
-                    <p className="hover:text-[#dede50] transition-all duration-200 ease-in">Signup</p>
-                  </Link>
-                </div>
+              <div className="flex font-poppins font-semibold space-x-10">
+                <Link to="/login">
+                  <p className="hover:text-[#dede50] transition-all duration-200 ease-in">
+                    Login
+                  </p>
+                </Link>
+                <Link to="/signup">
+                  <p className="hover:text-[#dede50] transition-all duration-200 ease-in">
+                    Signup
+                  </p>
+                </Link>
+              </div>
             )}
           </nav>
-
-          <div onClick={() => setPress(true)} className="sm:hidden cursor-pointer">
-            <GiHamburgerMenu className={pressed ? 'hidden' : 'block'} color="yellow" size="30" />
+          <div
+            onClick={() => setPress(true)}
+            className="sm:hidden cursor-pointer"
+          >
+            <GiHamburgerMenu
+              className={pressed ? "hidden" : "block"}
+              color="yellow"
+              size="30"
+            />
           </div>
-
-          <div onClick={() => setPress(false)} className="sm:hidden cursor-pointer">
-            <ImCross className={pressed ? 'block' : 'hidden'} color="yellow" size="25" />
+          <div
+            onClick={() => setPress(false)}
+            className="sm:hidden cursor-pointer"
+          >
+            <ImCross
+              className={pressed ? "block" : "hidden"}
+              color="yellow"
+              size="25"
+            />
           </div>
-
-
         </div>
       </div>
-      {pressed && ( /* Conditionally display the mobile menu */
+      {pressed /* Conditionally display the mobile menu */ && (
         <div className="flex transition-all duration-300 ease-in bg-yellow-400 flex-col items-center justify-center sm:hidden text-black font-bold font-poppins">
-          <Link className="py-4 hover:bg-burger w-full flex justify-center hover:text-yellow-400 transition-all duration-300 ease-in" to="/">
+          <Link
+            className="py-4 hover:bg-burger w-full flex justify-center hover:text-yellow-400 transition-all duration-300 ease-in"
+            to="/"
+          >
             Home
           </Link>
         </div>
       )}
     </div>
   );
-}
-export default Navbar
+};
+export default Navbar;
